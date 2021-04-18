a5796719016107fdaaf191e95b932da5de0a0487
Dawit Teklu <se.dawit.teklu@gmail.com>
2021-04-18 13:53:59 +0300

    creating database and tables from the database.py file:

--- database.py
+++ database.py
@@ -6,10 +6,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
+# database uri
+""""
+export DATABASE_URL=postgresql://xiyqayicucdjoo:0eccd743ed6c5e9a80251399d15c8ccc427d97dea0b6dde205880aa00eb6a58f@ec2-35-174-35-242.compute-1.amazonaws.com:5432/detas6rlmlsbrl
+"""
 def main():
 	#create table users
-	db.execute("""CREATE TABLE users(
+	db.execute("""CREATE TABLE IF NOT EXISTS users(
     				id serial primary key,
     				username text unique NOT NULL,
 					email text unique NOT NULL,
@@ -20,7 +23,7 @@ def main():
 	print("table users created\n")
 
 	#create table books
-	db.execute("""CREATE TABLE books(
+	db.execute("""CREATE TABLE IF NOT EXISTS books(
 				    id serial primary key,
 				    isbn char(10) unique NOT NULL,
 				    title text NOT NULL,
@@ -29,7 +32,7 @@ def main():
 
 	print("table books created\n")
 	#create table reviews
-	db.execute("""CREATE TABLE reviews(
+	db.execute("""CREATE TABLE IF NOT EXISTS reviews(
 				    id serial primary key,
 				    user_name text references users(username),
 				    book_id integer references books(id),
@@ -39,6 +42,14 @@ def main():
 
 	print("table reviews created\n")
 	db.commit()
+# my_file = open("books.csv")
+# books = csv.reader(my_file)
+# for isbn,title,author,year in books:
+#     db.execute("insert into books(isbn,title,author,year) values(:isbn,:title,:author,:year)",
+#     {"isbn": isbn, "title": title,"year": year,"author": author})
+#     print(f"Added book {title} by {author} in {year}: isbn: {isbn}")
+# print("Books entered successfully")
+# db.commit()
     	 		 
 if __name__ == "__main__":
     main()
